update spectrum level notebook and fix white_noise generator with spectrum level normalisation

data_creation/ramp/functions.py
@@ -17,9 +17,12 @@ def linear_ramp_func(t: np.ndarray,
 
     Note that this ramp function ignores gamma_t and width
 
-    @arg t: time vector
-    @arg gamma_t: _unused_
-    @arg width: _unused_
+    @arg t:
+        time vector
+    @arg gamma_t:
+        _unused_
+    @arg width:
+        _unused_
 
     @return:
     """
@@ -32,9 +35,12 @@ def cosine_squared_ramp_func(t: np.ndarray,
     """
     Creates a cosine squared ramp centered in gamma_t with defined width, relative to the time-vector t
 
-    @arg t: time vector
-    @arg gamma_t: center of ramp
-    @arg width: width of ramp
+    @arg t:
+        time vector
+    @arg gamma_t:
+        center of ramp
+    @arg width:
+        width of ramp
 
     @return:
     """
@@ -49,10 +55,18 @@ def _cosine_sum(t: np.ndarray,
     """
     Private helper used for cosine-sum ramps, i.e. Hann and Hamming
 
-    @arg t: time vector
-    @arg gamma_t: center of ramp
-    @arg width: width of ramp
+    Computed from:
+
+      - a0 - (1 - a0)cos(S{pi} * x / width)
+
+    @arg t:
+        time vector
+    @arg gamma_t:
+        center of ramp
+    @arg width:
+        width of ramp
     @arg a0:
+        Offset parameter, see function description for details
     @return:
     """
     x = t - (gamma_t - width / 2)
@@ -65,9 +79,12 @@ def hann_ramp_func(t: np.ndarray,
     """
     Ramp based on the Hann window function
 
-    @arg t: time vector
-    @arg gamma_t: center of ramp
-    @arg width: width of ramp
+    @arg t:
+        time vector
+    @arg gamma_t:
+        center of ramp
+    @arg width:
+        width of ramp
     @return:
     """
     return _cosine_sum(t=t, gamma_t=gamma_t, width=width, a0=0.5)
@@ -79,9 +96,12 @@ def hamming_ramp_func(t: np.ndarray,
     """
     Ramp based on the Hamming window function
 
-    @arg t: time vector
-    @arg gamma_t: center of ramp
-    @arg width: width of ramp
+    @arg t:
+        time vector
+    @arg gamma_t:
+        center of ramp
+    @arg width:
+        width of ramp
     @return:
     """
     return _cosine_sum(t=t, gamma_t=gamma_t, width=width, a0=25.0 / 46.0)
@@ -94,9 +114,12 @@ def gaussian_ramp_func(t: np.ndarray,
     Ramp function based on the gaussian gate used by
     U{Schneider and Hamstra, 1999 <https://asa.scitation.org/doi/10.1121/1.427062>}
 
-    @arg t: time vector
-    @arg gamma_t: center of ramp
-    @arg width: width of ramp
+    @arg t:
+        time vector
+    @arg gamma_t:
+        center of ramp
+    @arg width:
+        width of ramp
     @return:
     """
     gamma_t += width / 2

data_creation/stimulus/noise.py
@@ -2,6 +2,8 @@ from typing import Protocol, List, Annotated, Union
 
 import numpy as np
 
+from data_creation.calibration.transform import a2spl
+from data_creation.calibration.spectrum import normalise_by_spectrum_level
 from data_creation.time.time import get_sampling_frequency
 
 
@@ -9,20 +11,47 @@ class WhiteNoiseFunction(Protocol):
     def __call__(self, t: int) -> np.ndarray: ...
 
 
-def uniform_white_noise(n: int) -> np.ndarray:
+def uniform_white_noise_generator(n: int) -> np.ndarray:
     return np.random.random(n) - 0.5
 
 
-def normal_distributed_white_noise(n: int) -> np.ndarray:
+def normal_distributed_white_noise_generator(n: int) -> np.ndarray:
     return np.random.randn(n)
 
 
-def white_noise_n(n: int, generator: WhiteNoiseFunction = uniform_white_noise) -> np.ndarray:
-    return generator(n)
+def white_noise_n(n: int,
+                  generator: WhiteNoiseFunction = uniform_white_noise_generator,
+                  dtype: type = float) -> np.ndarray:
+    return generator(n).astype(dtype=dtype)
 
 
-def white_noise(t, generator: WhiteNoiseFunction = uniform_white_noise) -> np.ndarray:
-    return white_noise_n(t.shape[0], generator)
+def white_noise(t: np.ndarray,
+                amp: float = 0.1,
+                generator: WhiteNoiseFunction = uniform_white_noise_generator,
+                dtype: type = float) -> np.ndarray:
+    """
+    Generate white noise of the length of the time vector
+
+    Example::
+
+        >>> from data_creation.time.time import generate_time_vector
+        >>> t = generate_time_vector(0.0, 1.0, 1000)
+        >>> x = white_noise(t, 0.5)
+
+    @arg t:
+        time vector
+    @arg amp:
+        amplitude
+    @arg generator:
+        generator function (most fulfill the L{WhiteNoiseFunction}.protocol)
+    @arg dtype:
+        datatype
+    @return:
+        noise signal
+    """
+    x = white_noise_n(t.shape[0], generator, dtype=dtype)
+    x = normalise_by_spectrum_level(t, x, a2spl(amp), None)
+    return x
 
 
 def broadband_noise_n(fs: Union[float, int] = 1e6,
@@ -38,10 +67,23 @@ def broadband_noise(t: np.ndarray,
                     amp: float = 0.1,
                     freq_range: Annotated[List[float], 2] = None,
                     dtype: type = float) -> np.ndarray:
+    """
+
+    @arg t:
+        time vector
+    @arg amp:
+        amplitude
+    @arg freq_range:
+        desired frequency range
+    @arg dtype:
+        desired datatype
+    @return:
+        noise signal
+    """
     n_t = t.shape[0]
     fs = get_sampling_frequency(None, t)
     if freq_range is None:
-        freq_range = np.array([1/8, 3/8]) * fs
+        freq_range = np.array([1 / 8, 3 / 8]) * fs
     if dtype is None:
         dtype = t.dtype
 
@@ -62,6 +104,5 @@ def broadband_noise(t: np.ndarray,
         signal += np.sin(f[i] * t + phase[i])
 
     signal *= amp
-    # signal /= np.max(np.abs(signal)) + eps
 
     return signal