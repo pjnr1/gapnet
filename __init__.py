8655b527a3ac936a436596263754fdb21b6e9b31
Jens Lindahl <jensctl@gmail.com>
2022-10-13 14:32:27 +0200

    update docs

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Welcome to the documentation of GapNet.
+
+Navigate to find the documentation for the specific module/function/constant/etc you need
+
+
+The project originates from the master thesis
+I{Simulating perceptual tasks with deep neural networks to improve diagnostics of hearing impairment}
+(U{full text <https://fulltext-gateway.cvt.dk/oafilestore?oid=624d7509610dfe20b13a2b14&targetid=624d75094aace9106e2219a5>}).
+"""
