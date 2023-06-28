f7fb773d4d50ea00c188ec75e59d5130e0b9bbc2
HEARING\jlindahl <jlindahl@falcom.net>
2023-06-28 16:54:38 +0200

    attempted move to src

--- setup.py
+++ setup.py
@@ -16,14 +16,10 @@ setup(
         author_email="jensctl@gmail.com",
         classifiers=[],
         keywords="",
-        package_dir={"": "src"},
+        package_dir={"": "."},
         packages=find_packages(where="."),
         python_requires=">=3.10, <4",
         install_requires=[],
-        extra_require={
-            "dev": [],
-            "test": [],
-        },
         package_data={},
         entry_points={},
         project_urls={
